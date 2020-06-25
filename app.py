31958b7d94365e5ac355435a4aa3381a35cf8914
Harikrishnan K <harikrishnanmaster@gmail.com>
2020-06-25 20:51:42 +0530

    first commit

--- app.py
+++ app.py
@@ -1,28 +1,46 @@
 import dash
 import dash_core_components as dcc
 import dash_html_components as html
-import plotly.graph_objs as go
 import pandas as pd
+import requests
 
-import plotly.graph_objs as go
-df = pd.read_csv('covid.csv')
-pv = pd.pivot_table(df, index=['location'], columns=["continent"], values=['total_cases'], aggfunc=sum, fill_value=0)
-trace1 = go.Bar(x=df["continent"], y=df[('total_cases')], name='Asia')
-trace2 = go.Bar(x=df["continent"], y=pv[('total_cases')], name='Eu')
-trace3 = go.Bar(x=df["continent"], y=pv[('total_cases')], name='Af')
-trace4 = go.Bar(x=df["continent"], y=pv[('total_cases')], name='NA')
+url = 'https://keralastats.coronasafe.live/hotspots.json'
+r = requests.get(url)
+x = r.json()
+df=pd.DataFrame(x['hotspots'])
+def generate_table(dataframe, max_rows=len(df)):
+    return html.Table(
+        # Header
+        [html.Tr([html.Th(col) for col in dataframe.columns])] +
+
+        # Body
+        [html.Tr([
+            html.Td(dataframe.iloc[i][col]) for col in dataframe.columns
+        ]) for i in range(min(len(dataframe), max_rows))]
+    )
+
 app = dash.Dash(__name__)
 server = app.server
+
 app.layout = html.Div(children=[
-    html.H1(children='Sales Funnel Report'),
-    html.Div(children='''National Sales Funnel Report.'''),
-    dcc.Graph(
-        id='example-graph',
-        figure={
-            'data': [trace1],
-            'layout':
-            go.Layout(title='Order Status by Customer', barmode='stack')
-        })
+    html.H4(children='Hot spots in Kerala'),
+    dcc.Dropdown(id='dropdown', options=[
+        {'label': i, 'value': i} for i in df.district.unique()
+    ], multi=True, placeholder='Filter by state...'),
+    html.Div(id='table-container')
 ])
+
+@app.callback(
+    dash.dependencies.Output('table-container', 'children'),
+    [dash.dependencies.Input('dropdown', 'value')])
+def display_table(dropdown_value):
+    if dropdown_value is None:
+        return generate_table(df)
+
+    dff = df[df.district.str.contains('|'.join(dropdown_value))]
+    return generate_table(dff)
+
+app.css.append_css({"external_url": "https://codepen.io/chriddyp/pen/bWLwgP.css"})
+
 if __name__ == '__main__':
     app.run_server(debug=True)
