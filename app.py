ed8c5fc0e561fb5636048acba498d8dc53589993
Harikrishnan K <harikrishnanmaster@gmail.com>
2020-06-28 23:08:13 +0530

    updatedd

--- app.py
+++ app.py
@@ -11,12 +11,13 @@ df=pd.DataFrame(x['hotspots'])
 data=df.lsgd
 dist=df.district
 list=[]
-for i,j in (data,dist):
-    sep = ' '
-    value = data[i]
-    dist = dist[j]
-    new_value= value.split(sep, 1)[0] +  ',India'
-    list.append(new_value)
+for i in range (0,len(data)):
+	for j in range (0,len(dist)):
+    		sep = ' '
+    		value = data[i]
+    		dist = dist[j]
+    		new_value= value.split(sep, 1)[0] + dist.split(sep, 1)[0] + ',' + ',India'
+    	list.append(new_value)
 list_location=[]
 URL = "https://geocode.search.hereapi.com/v1/geocode"
 
