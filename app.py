b19f69259bb8905f5421b9677a313db59eb8d00a
Harikrishnan K <harikrishnanmaster@gmail.com>
2020-06-29 17:05:25 +0530

    updatedd

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
 import dash
-from dash.dependencies import Output, Event
+
 import dash_core_components as dcc
 import dash_html_components as html
 import plotly
 import random
 import plotly.graph_objs as go
-from collections import deque
+
 import dash_html_components as html
 import dash_core_components as dcc
 import requests
