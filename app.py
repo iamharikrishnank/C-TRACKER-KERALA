f7b9fcdbea8b651c4e05d8b9ab2c2b9e08df9c3e
Harikrishnan K <harikrishnanmaster@gmail.com>
2020-07-04 07:04:37 +0530

    graph added

--- app.py
+++ app.py
@@ -157,8 +157,8 @@ fig = go.Figure(data=[go.Bar(
 
 )])
 fig.update_layout(
-    width=800,
-    height=800,
+    width=1280,
+    height=1280,
     title="Quarantine Summary",
     xaxis_title="Districts",
     yaxis_title="Number of Peoples",
